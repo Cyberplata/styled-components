94cc54a1cec0501492e34972ce5559464fb763d1
PlatonKynin <kininplaton@gmail.com>
2024-01-22 20:59:29 +0300

    removed the Menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import './App.css';
 import styled from "styled-components";
-import {StyledBtn, SuperButton} from "./components/Button.styled";
+import {StyledBtn} from "./components/Button.styled";
 import {Link} from "./components/Link.styled";
-import {Menu} from "./components/Menu.styled";
 
 
 function App() {
     return (
         <div className="App">
-            <Menu>
-                <ul>
-                    <li><a href="">menu item 1</a></li>
-                    <li><a href="">menu item 2</a></li>
-                    <li><a href="">menu item 3</a></li>
-                </ul>
-            </Menu>
-
             <Box>
                 <StyledBtn>Hello</StyledBtn>
             </Box>
